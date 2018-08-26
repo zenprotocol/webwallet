33cacb5a7fd18ab3be975ddd3ae1072b95ce232d
Cremba <david@brima.it>
2018-08-26 12:28:49 +0300

    Fixed Paste from clipboard

--- src/components/PasteButton.js
+++ src/components/PasteButton.js
@@ -15,10 +15,14 @@ class PasteButton extends Component<Props> {
     text: 'Paste',
     className: '',
   }
-  onClick = (evt: SyntheticEvent<HTMLButtonElement>) => {
-    // TODO
-    alert('todo')
-    // this.props.onClick(clipboard.readText().trim(), evt)
+  onClick = ( evt: SyntheticEvent<HTMLButtonElement>) => {
+    navigator.clipboard.readText()
+  .then(text => {
+    this.props.onClick(text, evt)
+  })
+  .catch(err => {
+    console.error('Failed to read clipboard contents: ', err)
+  })
   }
   render() {
     const {
